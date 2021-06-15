75e0a85421e4cc7c1e17dd4b18c01702c96f3a49
nosrick <nosrick@gmail.com>
2021-06-15 08:42:20 +0100

    Adding config file

--- main-bot.js
+++ main-bot.js
@@ -4,6 +4,8 @@ const client = new Discord.Client();
 const dotenv = require('dotenv');
 dotenv.config();
 
+const config = require('./config.json');
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
